68295efb13929ceae18afdc98ba2ee22c16d32ac
Manohar <manohar.goud123@gmail.com>
2025-08-26 16:42:27 +0530

    Initial commit for consumer project

---
+++ inventoryclient/inventory-client/src/test/java/com/example/inventory_client/InventoryClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inventory_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
